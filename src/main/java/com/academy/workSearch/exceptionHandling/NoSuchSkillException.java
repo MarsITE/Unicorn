a9b7ce8b3198318257da5d9755b88dad9c2be7b8
Liliana_Chevka <lilisla83@gmail.com>
2021-04-05 01:34:35 +0300

    Added new exceptions, changes in UserDAOImpl,UserController

--- src/main/java/com/academy/workSearch/exceptionHandling/NoSuchSkillException.java
+++
@@ -1,7 +0,0 @@
-package com.academy.workSearch.exceptionHandling;
-
-public class NoSuchSkillException extends RuntimeException{
-    public NoSuchSkillException(String message) {
-        super(message);
-    }
-}
