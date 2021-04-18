46e6eb48ee002a95283ca05785d832c7cebe8f40
Chupyra Serhii <ch.sergijgmail.com>
2021-04-19 01:47:03 +0300

    example of usage of tokenHelper.isCurrentUserAdmin

--- front-end/src/app/components/admin/admin.component.ts
+++ front-end/src/app/components/admin/admin.component.ts
@@ -7,6 +7,7 @@ import { SkillService } from '../../common/services/skill.service';
 import { ConfirmComponent } from '../modals/confirm/confirm.component';
 import { MatDialog } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
+import { TokenHelper } from 'src/app/common/helper/token.helper';
 
 @Component({
   selector: 'app-admin',
@@ -19,7 +20,7 @@ export class AdminComponent implements OnInit, OnDestroy {
   private subscriptions: Subscription[] = [];
 
   constructor(private skillService: SkillService, private router: Router, private dialog: MatDialog,
-              private toastr: ToastrService) {
+              private toastr: ToastrService, private tokenHelper: TokenHelper) {
   }
 
   ngOnDestroy(): void {
@@ -29,26 +30,29 @@ export class AdminComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.getSkills();
+    this.tokenHelper.isCurrentUserAdmin ? this.getSkillsDetails() : this.getSkills();
   }
 
   private getSkills(): void {
+    this.subscriptions.push(this.skillService.getSkills().subscribe(
+      (response: Skill[]) => {
+        this.skills = response.sort((a: Skill) => (a.enabled ? 1 : -1));
+      },
+      (error) => { this.toastr.error('Skills was not received!', 'Error!'); }
+    ));
+  }
+
+  private getSkillsDetails(): void {
     this.subscriptions.push(this.skillService.getSkillsDetails().subscribe(
       (response: Skill[]) => {
         this.skills = response.sort((a: Skill) => (a.enabled ? 1 : -1));
       },
-      (error) => {
-        console.log("error", error);
-        this.toastr.error('Skills was not received!', 'Oops!');
-      },
-      () => {
-        console.log("complete");
-        console.log(this.skills);
-      }
+      (error) => { this.toastr.error('Skills was not received!', 'Error!'); }
     ));
   }
 
-  createSkill(){
+
+  createSkill(): void{
     const confirmDialog = this.dialog.open(ConfirmComponent, {
       data: {
         title: 'Confirm you want to add new skill',
