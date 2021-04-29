80113bbfdde22ce52aed4ec966e3e40b191cde9f
dianayavor <dianayavorsks7@gmail.com>
2021-04-29 14:44:17 +0300

    add services and repositories

---
+++ src/main/java/com/academy/workSearch/dao/ProjectUserInfoDAO.java
@@ -0,0 +1,6 @@
+package com.academy.workSearch.dao;
+
+import com.academy.workSearch.model.ProjectUserInfo;
+
+public interface ProjectUserInfoDAO extends CrudDAO<ProjectUserInfo> {
+}
---
+++ src/main/java/com/academy/workSearch/service/implementation/ProjectUserInfoServiceImpl.java
@@ -0,0 +1,8 @@
+package com.academy.workSearch.service.implementation;
+
+import com.academy.workSearch.service.ProjectUserInfoService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectUserInfoServiceImpl implements ProjectUserInfoService {
+}
