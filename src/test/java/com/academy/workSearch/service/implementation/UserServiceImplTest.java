2a6c879f9ecab9e8e949f08d5aa08901dd0cdf6c
dianayavor <lana2345>
2021-05-10 12:23:08 +0300

    add tests

--- src/test/java/com/academy/workSearch/service/implementation/UserServiceImplTest.java
+++ src/test/java/com/academy/workSearch/service/implementation/UserServiceImplTest.java
@@ -3,10 +3,12 @@ package com.academy.workSearch.service.implementation;
 import com.academy.workSearch.dao.RoleDAO;
 import com.academy.workSearch.dao.UserDAO;
 import com.academy.workSearch.dao.UserInfoDAO;
+import com.academy.workSearch.dto.UserAuthDTO;
 import com.academy.workSearch.dto.UserRegistrationDTO;
 import com.academy.workSearch.model.Role;
 import com.academy.workSearch.model.User;
 import com.academy.workSearch.model.UserInfo;
+import com.academy.workSearch.model.enums.AccountStatus;
 import com.academy.workSearch.service.JwtService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +16,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.*;
@@ -21,7 +24,8 @@ import java.util.*;
 import static com.academy.workSearch.dto.mapper.UserMapper.USER_MAPPER;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
@@ -75,19 +79,24 @@ class UserServiceImplTest {
         userInfo.setShowInfo(true);
         user.setPassword("$2a$10$8R//8mSYBIn4hpFM2pV8k.Ye7nPCDQNXtDGZz7FOsxjK.U7B6RYpq");
         user.setRegistrationToken("uyvvdbunisndbhvfbnjdwmjhvfbjdnmvbdfnwdm");
+        user.setAccountStatus(AccountStatus.NOT_ACTIVE);
 
-        when(userDAO.save(user)).thenReturn(user);
+        when(userDAO.save(any())).thenReturn(user);
         when(roleDAO.getByName("WORKER")).thenReturn(Optional.of(new Role(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb4cb5f4"), "WORKER")));
         when(roleDAO.getByName("EMPLOYER")).thenReturn(Optional.of(new Role(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb1cb5f4"), "EMPLOYER")));
         when(userInfoDAO.saveAndGetId(userInfo)).thenReturn(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb2cb5f4"));
         when(passwordEncoder.encode("111111")).thenReturn("$2a$10$8R//8mSYBIn4hpFM2pV8k.Ye7nPCDQNXtDGZz7FOsxjK.U7B6RYpq");
         when(jwtService.generateRegistrationToken(user.getEmail())).thenReturn("uyvvdbunisndbhvfbnjdwmjhvfbjdnmvbdfnwdm");
 
+        UserServiceImpl userServiceImpl = spy(userService);
+        doNothing().when(userServiceImpl).sendMessageWithActivationLink(any());
+
         UserRegistrationDTO userRegistrationDTO = new UserRegistrationDTO();
         userRegistrationDTO.setEmail("anna@gmail.com");
         userRegistrationDTO.setPassword("111111");
         userRegistrationDTO.setIsEmployer(true);
-        assertEquals("anna@gmail.com", userService.save(userRegistrationDTO).getEmail(), "The user returned was not the same as the mock");
+        UserAuthDTO expected = userServiceImpl.save(userRegistrationDTO);
+        assertEquals("anna@gmail.com", expected.getEmail(), "The user returned was not the same as the mock");
     }
 
 }
