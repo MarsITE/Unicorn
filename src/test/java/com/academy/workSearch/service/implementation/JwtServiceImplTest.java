22a8c55728a0e292887821c29630379a8fdd133b
dianayavor <lana2345>
2021-05-12 12:44:02 +0300

    add more tests for jwt service

--- src/test/java/com/academy/workSearch/service/implementation/JwtServiceImplTest.java
+++ src/test/java/com/academy/workSearch/service/implementation/JwtServiceImplTest.java
@@ -3,6 +3,7 @@ package com.academy.workSearch.service.implementation;
 import com.academy.workSearch.dao.RoleDAO;
 import com.academy.workSearch.model.Role;
 import com.academy.workSearch.model.User;
+import com.academy.workSearch.model.UserInfo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,10 +11,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class JwtServiceImplTest {
@@ -25,16 +30,91 @@ public class JwtServiceImplTest {
 
     @Test
     void checkGeneratingAccessToken() {
-//        User user = new User();
-//        user.setUserId(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb4cb5f7"));
-//        user.getUserInfo().setUserInfoId(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb4cb5f8"));
-//        user.setEmail("a@gmail.com");
-//        Set<Role> roles = new HashSet<>();
-//        roles.add(new Role(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb4cb5f4"), "WORKER"));
-//        roles.add(new Role(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb1cb5f4"), "EMPLOYER"));
-//        user.setRoles(roles);
-//
-//
-//        assertEquals("a@gmail.com", jwtService.generateAccessToken(user), "Token is not create");
+        User user = new User();
+        user.setUserId(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb4cb5f7"));
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUserInfoId(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb4cb5f8"));
+        user.setUserInfo(userInfo);
+        user.setEmail("a@gmail.com");
+        Set<Role> roles = new HashSet<>();
+        Role worker = new Role(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb4cb5f4"), "WORKER");
+        roles.add(worker);
+        user.setRoles(roles);
+
+        when(roleDAO.getByName(any())).thenReturn(Optional.of(worker));
+
+        assertNotNull(jwtService.generateAccessToken(user), "Token is not create");
     }
+
+    @Test
+    void checkGeneratingRefreshsToken() {
+        assertNotNull(jwtService.generateRefreshToken("a@gmail.com"), "Token is not create");
+    }
+
+    @Test
+    void checkGeneratingRegistrationToken() {
+        assertNotNull(jwtService.generateRegistrationToken("a@gmail.com"), "Token is not create");
+    }
+
+
+    @Test
+    void getIdFromToken() {
+        User user = new User();
+        user.setUserId(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb4cb5f7"));
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUserInfoId(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb4cb5f8"));
+        user.setUserInfo(userInfo);
+        user.setEmail("a@gmail.com");
+        Set<Role> roles = new HashSet<>();
+        Role worker = new Role(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb4cb5f4"), "WORKER");
+        roles.add(worker);
+        user.setRoles(roles);
+
+        when(roleDAO.getByName(any())).thenReturn(Optional.of(worker));
+
+        String token = jwtService.generateAccessToken(user);
+
+        assertEquals("f6cea10a-2f9d-4feb-82ba-b600bb4cb5f7", jwtService.getUserId(token), "bad token");
+    }
+
+    @Test
+    void getEmailFromToken() {
+        User user = new User();
+        user.setUserId(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb4cb5f7"));
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUserInfoId(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb4cb5f8"));
+        user.setUserInfo(userInfo);
+        user.setEmail("a@gmail.com");
+        Set<Role> roles = new HashSet<>();
+        Role worker = new Role(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb4cb5f4"), "WORKER");
+        roles.add(worker);
+        user.setRoles(roles);
+
+        when(roleDAO.getByName(any())).thenReturn(Optional.of(worker));
+
+        String token = jwtService.generateAccessToken(user);
+
+        assertEquals("a@gmail.com", jwtService.getUsername(token), "bad token");
+    }
+
+    @Test
+    void getRoles() {
+        User user = new User();
+        user.setUserId(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb4cb5f7"));
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUserInfoId(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb4cb5f8"));
+        user.setUserInfo(userInfo);
+        user.setEmail("a@gmail.com");
+        Set<Role> roles = new HashSet<>();
+        Role worker = new Role(UUID.fromString("f6cea10a-2f9d-4feb-82ba-b600bb4cb5f4"), "WORKER");
+        roles.add(worker);
+        user.setRoles(roles);
+
+        when(roleDAO.getByName(any())).thenReturn(Optional.of(worker));
+
+        String token = jwtService.generateAccessToken(user);
+
+        assertEquals(roles.size(), jwtService.getRoles(token).size(), "bad token");
+    }
+
 }
